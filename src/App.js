549a0252f8bfdca8ed7f8dc0cc0534b871fcb0a3
dharma-acha <100557655+dharma-acha@users.noreply.github.com>
2022-09-07 20:22:13 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
-
+import React, { useState } from "react";
+function App() {
+    const [color, setColor] = useState("red")
+    function ChangeColor() {
+        setColor("blue")
+        if (color === "blue") {
+            setColor("red")
+        }
+    }
+    window.onload=function(){
+        const button= document.querySelector("button");
+        const checkbox=document.querySelector("#check");
+           let disableSubmit=function(e){
+                button.disabled=this.checked
+            }
+        checkbox.addEventListener("change",disableSubmit);
+    }
+    return (
+        <div>
+            <button onClick={ChangeColor} style={{ backgroundColor: color }}>{color === "red" ? ("change to blue") : ("change to red")}</button>
+            <br /><br />
+            <input  type="checkbox" id="check" name="" />
+        </div>
+    )
+}
+export default App;
